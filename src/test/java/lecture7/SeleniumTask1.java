54b75c6e6fbb05da1ac3815fc24d8b5e8c961f90
Emilija <emilijahaikina@yahoo.com>
2022-03-30 13:43:00 +0100

    Fixed bugs

--- src/test/java/lecture7/SeleniumTask1.java
+++ src/test/java/lecture7/SeleniumTask1.java
@@ -18,6 +18,9 @@ public class SeleniumTask1 {
         driver.get("https://www.aliexpress.com/");
         driver.manage().window().maximize();
 
+        driver.findElement(By.xpath(("//img[@class=\"_24EHh\"]"))).click();
+        driver.findElement(By.xpath(("//img[@class=\"btn-close\"]"))).click();
+
         driver.findElement(By.id( "search-key")).sendKeys("tattoo");
         driver.findElement(By.xpath(("//input[@class=\"search-button\"]"))).click();
 
@@ -26,8 +29,8 @@ public class SeleniumTask1 {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        driver.findElement(By.xpath(("//input[@control-id=\"ControlID-3\"]"))).sendKeys("10");
-        driver.findElement(By.xpath(("//input[@control-id=\"ControlID-4\"]"))).sendKeys("20");
+        driver.findElement(By.xpath(("//input[@placeholder=\"min\"]"))).sendKeys("10");
+        driver.findElement(By.xpath(("//input[@placeholder=\"max\"]"))).sendKeys("20");
         driver.findElement(By.xpath(("//a[@class=\"ui-button narrow-go\"]"))).click();
 
         try {
